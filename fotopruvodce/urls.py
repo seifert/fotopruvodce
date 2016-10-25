659160fc39b48057b7bee620b23bec8449d7623c
Jan Seifert <jan.seifert@fotkyzcest.net>
2016-10-25 21:12:07 +0200

    add core app

--- fotopruvodce/urls.py
+++ fotopruvodce/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from fotopruvodce.core import views as core_views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', core_views.homepage, name='homepage'),
 ]
