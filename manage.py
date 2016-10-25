1260677cd65d3e73c3c52feea3b915002884e840
Jan Seifert <jan.seifert@fotkyzcest.net>
2016-10-25 21:03:09 +0200

    application skeleton

---
+++ manage.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import fotopruvodce
+
+if __name__ == "__main__":
+    fotopruvodce.main()
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='fotopruvodce',
+    version='0.0.1',
+    author='Jan Seifert',
+    author_email='jan.seifert@fotkyzcest.net',
+    description=(
+        'Fotografická diskusní aplikace poskytujici prostor fotografům '
+        'libovolného ražení k diskusi svých problémů, či sdělování '
+        'zkušenosti.'
+    ),
+    license='BSD',
+    platforms=['any'],
+    url='http://fotopruvodce.cz/',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: Other/Proprietary License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Framework :: Django',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'django>=1.10,<1.11',
+        'Pillow',
+    ],
+    entry_points={
+        'console_scripts': [
+            'manage-fotopruvodce = fotopruvodce:main',
+        ]
+    }
+)
