62d2bef906b54d334d0f795edf1d634fe18be0e6
Jan Seifert <jan.seifert@fotkyzcest.net>
2016-10-27 23:26:47 +0200

    catch user exists error

--- fotopruvodce/registration/views.py
+++ fotopruvodce/registration/views.py
@@ -1,6 +1,7 @@
 
 from django.contrib.auth import login
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.shortcuts import render, redirect
 
 from fotopruvodce.registration.forms import Register
@@ -11,15 +12,19 @@ def registration(request):
         form = Register(request.POST)
 
         if form.is_valid():
-            user = User.objects.create_user(
-                username=form.cleaned_data['username'],
-                email=form.cleaned_data['email'],
-                password=form.cleaned_data['password1'],
-                first_name=form.cleaned_data['first_name'],
-                last_name=form.cleaned_data['last_name'],
-            )
-            login(request, user)
-            return redirect("homepage")
+            try:
+                user = User.objects.create_user(
+                    username=form.cleaned_data['username'],
+                    email=form.cleaned_data['email'],
+                    password=form.cleaned_data['password1'],
+                    first_name=form.cleaned_data['first_name'],
+                    last_name=form.cleaned_data['last_name'],
+                )
+            except IntegrityError:
+                form.add_error('username', 'Uživatel již existuje')
+            else:
+                login(request, user)
+                return redirect('homepage')
     else:
         form = Register()
 
