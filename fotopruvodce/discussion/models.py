341fc3264db557c1a0641b9cc46e93beff78e802
Jan Seifert <jan.seifert@fotkyzcest.net>
2016-10-31 22:59:25 +0100

    discussion - add tree of the comments

--- fotopruvodce/discussion/models.py
+++ fotopruvodce/discussion/models.py
@@ -18,3 +18,41 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return reverse('comment-detail', args=[self.id])
+
+    @property
+    def children(self):
+        """
+        Children of the this comment.
+        """
+        return Comment.objects.filter(parent=self).order_by('timestamp')
+
+    def get_main_comment(self, _comment=None):
+        """
+        Return main (first) comment for discussion thread identified by
+        this comment.
+        """
+        if _comment is None:
+            _comment = self
+
+        if _comment.parent is None:
+            return _comment
+        else:
+            return self.get_main_comment(_comment.parent)
+
+    def get_tree(self, _comment=None, _tree=None, _level=0):
+        """
+        Return tree of the comments for discussion thread identified by
+        this comment.
+        """
+        if _comment is None:
+            _comment = self.get_main_comment(self)
+        if _tree is None:
+            _tree = []
+
+        _tree.append((_comment, _level))
+
+        _level += 1
+        for child in _comment.children:
+            self.get_tree(child, _tree, _level)
+
+        return _tree
