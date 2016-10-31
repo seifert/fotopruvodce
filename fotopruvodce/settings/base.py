bcb5ad6f1b825c143cf7faa30955f67f081b90f8
Jan Seifert <jan.seifert@fotkyzcest.net>
2016-10-31 23:05:54 +0100

    fixed collected static path, gitignore

--- fotopruvodce/settings/base.py
+++ fotopruvodce/settings/base.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, os.pardir, 'collected')
+STATIC_ROOT = os.path.join(BASE_DIR, os.pardir, 'static')
 
 
 # Auth
