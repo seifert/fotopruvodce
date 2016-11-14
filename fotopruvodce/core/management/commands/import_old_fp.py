f00feb105e90921315b03d3f236e3d3b102ed2c5
Jan Seifert <jan.seifert@fotkyzcest.net>
2016-11-14 23:25:51 +0100

    import discussion

--- fotopruvodce/core/management/commands/import_old_fp.py
+++ fotopruvodce/core/management/commands/import_old_fp.py
@@ -1,9 +1,34 @@
 
+from datetime import datetime, date, time
+from functools import lru_cache
+
+import pytz
+
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
-from django.core.validators import ValidationError, validate_email
+from django.core.validators import ValidationError, validate_email, validate_ipv46_address
 from django.db import connections, transaction
 from django.db.utils import IntegrityError
+
+from fotopruvodce.discussion.models import Comment, AnonymousComment
+
+User = get_user_model()
+
+
+@lru_cache(maxsize=1024)
+def get_user(username):
+    try:
+        return User.objects.get(username=username)
+    except User.DoesNotExist:
+        return None
+
+
+@lru_cache(maxsize=1024)
+def get_comment(id):
+    try:
+        return Comment.objects.get(id=id)
+    except Comment.DoesNotExist:
+        return None
 
 
 class Command(BaseCommand):
@@ -18,19 +43,22 @@ class Command(BaseCommand):
         conn = connections['default']
         conn_old = connections['old']
 
+        # Anonymous user for unregistered users
+        try:
+            anonymous_user = User.objects.get(username='Anonymous')
+        except User.DoesNotExist:
+            anonymous_user = User(
+                username='Anonymous', is_active=False,
+                is_staff=False, is_superuser=False)
+            anonymous_user.save()
+
         with conn_old.cursor() as cursor_old:
 
             # Users
             self.stdout.write('Import users')
-            with conn.cursor() as cursor:
-                cursor.execute(
-                    'ALTER TABLE auth_user MODIFY username varchar(150) '
-                    'CHARACTER SET utf8 COLLATE utf8_bin'
-                )
-            User = get_user_model()
             counter = 0
             cursor_old.execute(
-                'SELECT id, passwd, jmeno, prijmeni, email, info from lidi'
+                'SELECT id, passwd, jmeno, prijmeni, email, info FROM lidi'
             )
             for row in cursor_old.fetchall():
                 id, passwd, jmeno, prijmeni, email, info = row
@@ -57,6 +85,80 @@ class Command(BaseCommand):
                     self.stdout.write(self.style.ERROR('%s %r' % (e, row)))
 
                 counter += 1
+
+            self.stdout.write(self.style.SUCCESS(
+                'Successfully imported %d rows' % counter))
+
+            # Discussion
+            self.stdout.write('Import discussion')
+            counter = 0
+            cursor_old.execute(
+                'SELECT id, nazev, zprava, tatka, thread, autor, email, '
+                'den, cas, ip, registered FROM clanky ORDER BY id'
+            )
+            # WHERE thread IN (137269, 137249)
+            for row in cursor_old.fetchall():
+                (id, nazev, zprava, tatka, thread, autor,
+                 email, den, cas, ip, registered) = row
+
+                # user
+                if registered:
+                    user = get_user(autor)
+                    if not user:
+                        self.stdout.write(self.style.ERROR(
+                            "Comment {} - convert '{}' to anonymous user".format(id, autor)))
+                        user = anonymous_user
+                else:
+                    user = anonymous_user
+                # ip
+                if ip:
+                    try:
+                        validate_ipv46_address(ip)
+                    except ValidationError:
+                        self.stdout.write(self.style.ERROR(
+                            "Comment {} - invalid IP {}".format(id, ip)))
+                        ip = '0.0.0.0'
+                else:
+                    ip = '0.0.0.0'
+                # timestamp
+                if den is None:
+                    den = date(100, 1, 1)
+                if cas is None:
+                    cas = time.min
+                timestamp = pytz.timezone(
+                    "Europe/Prague"
+                ).localize(
+                    datetime.combine(den, cas), is_dst=None
+                )
+                # parent
+                if tatka != id:
+                    parent = get_comment(tatka)
+                    if not parent:
+                        self.stdout.write(self.style.ERROR(
+                            "Comment {} - parent {} doesn't exist, "
+                            "try to find thread".format(id, tatka)
+                        ))
+                        parent = get_comment(thread)
+                        if not parent:
+                            self.stdout.write(self.style.ERROR(
+                                "Comment {} - make orphan".format(id)
+                            ))
+                            parent = None
+                else:
+                    parent = None
+
+                comment = Comment(
+                    id=id, title=nazev, content=zprava, timestamp=timestamp,
+                    ip=ip, user=user, parent=parent)
+                comment.save(force_insert=True)
+                if user == anonymous_user:
+                    anonymous = AnonymousComment(
+                        comment=comment, author=autor, email=email)
+                    anonymous.save()
+
+                counter += 1
+                if not counter % 1000:
+                    self.stdout.write(str(counter))
 
             self.stdout.write(self.style.SUCCESS(
                 'Successfully imported %d rows' % counter))
--- fotopruvodce/discussion/models.py
+++ fotopruvodce/discussion/models.py
@@ -2,13 +2,14 @@
 from django.conf import settings
 from django.db import models, DEFAULT_DB_ALIAS
 from django.urls import reverse
+from django.utils.timezone import now
 
 
 class Comment(models.Model):
 
     title = models.CharField(max_length=128, blank=False)
     content = models.TextField(blank=False)
-    timestamp = models.DateTimeField(blank=False, null=False, auto_now_add=True)
+    timestamp = models.DateTimeField(blank=False, null=False)
     ip = models.GenericIPAddressField(blank=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=False, null=False)
     parent = models.ForeignKey('self', blank=True, null=True)
@@ -23,6 +24,8 @@ class Comment(models.Model):
 
     def save(self, force_insert=False, force_update=False,
              using=DEFAULT_DB_ALIAS, update_fields=None):
+        if self.timestamp is None:
+            self.timestamp = now()
         # Reply - inherit thread id from parent and increase level
         if self.thread == 0 and self.parent:
             self.thread = self.parent.thread
@@ -35,3 +38,9 @@ class Comment(models.Model):
             self.thread = self.pk
             super().save(force_insert=False, force_update=True,
                          using=using, update_fields=['thread'])
+
+class AnonymousComment(models.Model):
+
+    comment = models.OneToOneField(Comment, on_delete=models.CASCADE, related_name='anonymous')
+    author = models.CharField(max_length=25)
+    email = models.CharField(max_length=64, blank=True)
