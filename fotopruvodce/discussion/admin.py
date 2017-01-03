1a18f1ec2b53b25a0acd3febba668b927293e7bc
Seifert, Jan <jan.seifert@firma.seznam.cz>
2017-01-03 22:26:45 +0100

    customize admin site

--- fotopruvodce/discussion/admin.py
+++ fotopruvodce/discussion/admin.py
@@ -1,7 +1,43 @@
 
 from django.contrib import admin
 
-from fotopruvodce.discussion.models import Comment
+from fotopruvodce.discussion.models import Comment, AnonymousComment
 
 
-admin.site.register(Comment)
+class AnonymousCommentInline(admin.TabularInline):
+
+    model = AnonymousComment
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        if obj is not None:
+            if not obj.is_anonymous:
+                return False
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    inlines = (AnonymousCommentInline,)
+    list_display = (
+        'id', 'title', 'timestamp', 'user', 'anonymous_user',
+        'parent_raw_id', 'thread')
+    list_display_links = ('id', 'title')
+    list_select_related = ('user',)
+    ordering = ('-timestamp',)
+    raw_id_fields = ('user', 'parent')
+    search_fields = ('title', 'user__username', 'anonymous__author')
+
+    def anonymous_user(self, obj):
+        return obj.anonymous.author
+    anonymous_user.short_description = 'Anonymous user'
+
+    def parent_raw_id(self, obj):
+        return obj.parent.id if obj.parent else None
+    parent_raw_id.short_description = 'Parent'
--- fotopruvodce/photos/admin.py
+++ fotopruvodce/photos/admin.py
@@ -4,7 +4,36 @@ from django.contrib import admin
 from fotopruvodce.photos.models import Section, Photo, Comment, Rating
 
 
-admin.site.register(Section)
-admin.site.register(Photo)
-admin.site.register(Comment)
-admin.site.register(Rating)
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+
+    list_display = ('title',)
+    ordering = ('title',)
+
+
+class CommentInline(admin.TabularInline):
+
+    fields = ('content', 'timestamp', 'user')
+    model = Comment
+    ordering = ('-timestamp',)
+    raw_id_fields = ('user',)
+
+
+class RatingInline(admin.TabularInline):
+
+    fields = ('rating', 'timestamp', 'user')
+    model = Rating
+    ordering = ('-timestamp',)
+    raw_id_fields = ('user',)
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    inlines = (CommentInline, RatingInline,)
+    list_display = ('id', 'title', 'timestamp', 'user', 'section', 'active')
+    list_display_links = ('id', 'title')
+    list_select_related = ('user',)
+    ordering = ('-timestamp',)
+    raw_id_fields = ('user',)
+    search_fields = ('title', 'user__username')
