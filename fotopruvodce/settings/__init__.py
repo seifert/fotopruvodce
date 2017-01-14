f6b24e7337be6b37d34f420a477a9010c5dcb6cc
Jan Seifert <jan.seifert@fotkyzcest.net>
2017-01-15 00:14:34 +0100

    debian packaging

--- fotopruvodce/settings/__init__.py
+++ fotopruvodce/settings/__init__.py
@@ -1,7 +1,14 @@
 
+import os
+
 from .base import *
 
 try:
     from .local import *
 except ImportError:
     pass
+
+if 'FOTOPRUVODCE_SECRET_KEY' in os.environ:
+    SECRET_KEY = os.environ['FOTOPRUVODCE_SECRET_KEY']
+
+del os
