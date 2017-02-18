7b6e36547b17bb69678517f5ac10a1889311ff69
Jan Seifert <jan.seifert@fotkyzcest.net>
2017-02-18 20:27:47 +0100

    add simple mardown editor

--- fotopruvodce/core/text.py
+++ fotopruvodce/core/text.py
@@ -1,16 +1,17 @@
 
 MARKDOWN_HELP_TEXT = (
-    "Pokud chcete text naformátovat, použijte buď základní HTML tagy "
-    "(p, ul, ol, b, i, br, a, …), nebo značkovací jazyk "
+    "Pokud chcete text naformátovat, použijte značkovací jazyk "
     "<a href=\"https://cs.wikipedia.org/wiki/Markdown\" target=\"_blank\">"
     "Markdown</a>:<br /><br />"
+    "<b>### Nadpis</b>&crarr;<br /><br />"
     "V rámci odstavce mohu zalamovat, ostavec bude &crarr;<br />"
     "ukončen až prázdným řádkem. Konec řádku &crarr;<br />"
-    "uprostřed&blank;&blank;&crarr;<br />"
-    "odstavce se udělá pomocí mezer na konci řádku.&crarr;<br />"
+    "uprostřed odstavce&blank;&blank;&blank;&crarr;<br />"
+    "se udělá pomocí několika mezer na konci řádku.&crarr;<br />"
     "&crarr;<br />"
     "&nbsp;&nbsp;&nbsp;* položka seznamu <b>*tučně*</b>&crarr;<br />"
     "&nbsp;&nbsp;&nbsp;* položka seznamu <i>**kurzívou**</i>&crarr;<br />"
+    "&nbsp;&nbsp;&nbsp;* položka seznamu <code>`neproporcionálně`</code>&crarr;<br />"
     "&crarr;<br />"
     "<a href="">[odkaz](http://example.com)</a>&crarr;"
 )
