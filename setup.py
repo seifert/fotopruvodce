aa42604abb4b8731e632b50c22f2fad9e4d0876c
Jan Seifert <jan.seifert@fotkyzcest.net>
2017-02-18 21:28:07 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fotopruvodce',
-    version='1.0.1',
+    version='1.0.2',
     author='Jan Seifert',
     author_email='jan.seifert@fotkyzcest.net',
     description=(
