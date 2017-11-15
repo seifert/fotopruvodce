0ea1791e3a16857abc8e644c2ac7e49bb6e453b6
Jan Seifert <jan.seifert@fotkyzcest.net>
2017-11-15 22:56:05 +0100

    fix html

--- fotopruvodce/core/text.py
+++ fotopruvodce/core/text.py
@@ -1,17 +1,17 @@
 
 MARKDOWN_HELP_TEXT = (
-    "Pokud chcete text naformátovat, použijte značkovací jazyk "
-    "<a href=\"https://cs.wikipedia.org/wiki/Markdown\" target=\"_blank\">"
-    "Markdown</a>:<br /><br />"
-    "<b>### Nadpis</b>&crarr;<br /><br />"
-    "V rámci odstavce mohu zalamovat, ostavec bude &crarr;<br />"
-    "ukončen až prázdným řádkem. Konec řádku &crarr;<br />"
-    "uprostřed odstavce&blank;&blank;&blank;&crarr;<br />"
-    "se udělá pomocí několika mezer na konci řádku.&crarr;<br />"
-    "&crarr;<br />"
-    "&nbsp;&nbsp;&nbsp;* položka seznamu <b>**tučně**</b>&crarr;<br />"
-    "&nbsp;&nbsp;&nbsp;* položka seznamu <i>*kurzívou*</i>&crarr;<br />"
-    "&nbsp;&nbsp;&nbsp;* položka seznamu <code>`neproporcionálně`</code>&crarr;<br />"
-    "&crarr;<br />"
-    "<a href="">[odkaz](http://example.com)</a>&crarr;"
+    'Pokud chcete text naformátovat, použijte značkovací jazyk '
+    '<a href="https://cs.wikipedia.org/wiki/Markdown" target="_blank">'
+    'Markdown</a>:<br><br>'
+    '<b>### Nadpis</b>&crarr;<br><br>'
+    'V rámci odstavce mohu zalamovat, ostavec bude &crarr;<br>'
+    'ukončen až prázdným řádkem. Konec řádku &crarr;<br>'
+    'uprostřed odstavce&blank;&blank;&blank;&crarr;<br>'
+    'se udělá pomocí několika mezer na konci řádku.&crarr;<br>'
+    '&crarr;<br>'
+    '&nbsp;&nbsp;&nbsp;* položka seznamu <b>**tučně**</b>&crarr;<br>'
+    '&nbsp;&nbsp;&nbsp;* položka seznamu <i>*kurzívou*</i>&crarr;<br>'
+    '&nbsp;&nbsp;&nbsp;* položka seznamu <code>`neproporcionálně`</code>&crarr;<br>'
+    '&crarr;<br>'
+    '<a href="">[odkaz](http://example.com)</a>&crarr;'
 )
