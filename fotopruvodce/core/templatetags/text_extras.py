2107980f272ca0bd0cd45a77c0dda7b11cbedb78
Jan Seifert <jan.seifert@fotkyzcest.net>
2017-12-20 21:31:26 +0100

    add startswith template filter

--- fotopruvodce/core/templatetags/text_extras.py
+++ fotopruvodce/core/templatetags/text_extras.py
@@ -6,3 +6,8 @@ from fotopruvodce.utils.text import raw_text_to_html
 register = template.Library()
 
 register.filter('raw_text_to_html', raw_text_to_html)
+
+
+@register.filter
+def startswith(s, prefix):
+    return s.startswith(prefix)
