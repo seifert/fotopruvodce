b4d9d337de004d6fc37ee14cddfe4649241b9e6a
Jan Seifert <jan.seifert@fotkyzcest.net>
2019-09-01 14:09:22 +0200

    upgrade to django 1.11

---
+++ fotopruvodce/core/staticfiles.py
@@ -0,0 +1,8 @@
+
+from django.contrib.staticfiles import storage
+
+
+class ManifestStaticFilesStorage(storage.ManifestStaticFilesStorage):
+
+    max_post_process_passes = 50
+    manifest_strict = False
